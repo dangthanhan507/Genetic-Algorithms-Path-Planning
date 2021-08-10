3181809027a82944709ddf49c50cf5a3d9a11940
dangthanhan507 <dangthanhan507@gmail.com>
2021-08-10 00:12:22 -0700

    changed parameters

--- GA/ga_pygame.py
+++ GA/ga_pygame.py
@@ -10,7 +10,7 @@ pygame.init()
 obstacles, shape, MAP = map_lib.map2()
 
 
-ga = GA_Solver(MAP, generations=50, parents_mating=20, solutions_per_pop=50, num_nodes=8, thresh=(0,16), mutation_percent=10)
+ga = GA_Solver(MAP, generations=40, parents_mating=4, solutions_per_pop=50, num_nodes=8, thresh=(0,16), mutation_percent=10)
 ga.train_ga()
 solution = ga.convert_solution()
 
